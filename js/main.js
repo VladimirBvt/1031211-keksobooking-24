d72404e8b3f3160ab63ffd92cfaea271c10286e6
Vladimir_Bavtenko <batonich84@mail.ru>
2021-10-06 09:34:32 +0300

    Генерирует массив из 10 различных объектов. Каждый объект - объявление.

--- js/main.js
+++ js/main.js
@@ -32,3 +32,94 @@ function getRandomArbitrary(min, max, decimalPlaces) {
   return rounding(result, decimalPlaces);
 }
 getRandomArbitrary();
+
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+
+const TIMES = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+// функция принимающая массив и возвращающая случайный элемент этого массива.
+/*const getRandomArrayElement = (elements) => elements[getRandomIntInclusive(0, elements.length - 1)];
+getRandomArrayElement();*/
+
+// функция, принимающая массив и возвращающая этот массив с удаленными из него повторяющимися элементами.
+const getArrayWithUniqueElements = (array) => {
+  const set = new Set(array);
+  return array = Array.from(set);
+};
+
+// функция, возвращающая массив неповторяющихся двузначных номеров пользователей в рандомизированном порядке (для отображения аватара от 01 до 10).
+const getUserNumbers = () => {
+  const userNumbers = [];
+  let arrayNumbers = [];
+  let set;
+  while (arrayNumbers.length < 10) {
+    const number = getRandomIntInclusive(1, 10);
+    const userNumber = number < 10 ? `0${number}` : `${number}`;
+    userNumbers.push(userNumber);
+    set = new Set(userNumbers);
+    arrayNumbers = Array.from(set);
+  }
+  return arrayNumbers;
+};
+const ArrayUserNumbers = getUserNumbers();
+
+const SIMILAR_ANNOUNCEMENT_COUNT = 10;
+
+const createAnnouncement = () => {
+  const getRandomPhotosLength = getRandomIntInclusive(0, PHOTOS.length);
+  const getRandomArrayPhotos = () => PHOTOS[getRandomIntInclusive(0, PHOTOS.length - 1)];
+  const getRandomFeaturesLength = getRandomIntInclusive(0, FEATURES.length);
+  const getRandomArrayFeatures = () => FEATURES[getRandomIntInclusive(0, FEATURES.length - 1)];
+  const getRandomLat = () => getRandomArbitrary(35.65000, 35.70000, 5);
+  const getRandomLng = () => getRandomArbitrary(139.70000, 139.80000, 5);
+
+  return {
+    author: {
+      avatar: `img/avatars/user${ArrayUserNumbers.shift()}.png`,
+    },
+    offer: {
+      title: 'Заголовок предложения',
+      address: `${getRandomLat()}, ${getRandomLng()}`,
+      price: getRandomIntInclusive(0, Number.MAX_SAFE_INTEGER),
+      type: TYPES[getRandomIntInclusive(0, TYPES.length - 1)],
+      rooms: getRandomIntInclusive(0, Number.MAX_SAFE_INTEGER),
+      guests: getRandomIntInclusive(0, Number.MAX_SAFE_INTEGER),
+      checkin: TIMES[0],
+      checkout: TIMES[2],
+      features: getArrayWithUniqueElements(Array.from({length: getRandomFeaturesLength}, getRandomArrayFeatures)),
+      description: 'описание помещения',
+      photos: Array.from({length: getRandomPhotosLength}, getRandomArrayPhotos),
+    },
+    location: {
+      lat: getRandomLat(),
+      lng: getRandomLng(),
+    },
+  };
+};
+
+Array.from({length: SIMILAR_ANNOUNCEMENT_COUNT}, createAnnouncement);
