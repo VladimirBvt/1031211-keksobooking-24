39ede9881a1141bdecbcde91d6580bb25f8e5f90
Vladimir_Bavtenko <batonich84@mail.ru>
2021-10-25 16:41:29 +0300

    Отображает фото жилья

--- js/popup.js
+++ js/popup.js
@@ -4,18 +4,11 @@ const cardsContainer = document.querySelector('.map__canvas');
 const similarCardTemplate = document.querySelector('#card').content.querySelector('.popup');
 const similarNotices = noticesRandom;
 const similarListFragment = document.createDocumentFragment();
-
-const createPhotos = function (someNotice) {
-  const getSources = someNotice.offer.photos;
-  getSources.forEach((source) => {
-    const image = document.createElement('img');
-    image.src = source;
-    image.alt = 'Фото жилья';
-    image.classList.add('popup__photo');
-    return image;
-  });
+const clearPhotosContainer = function () {
+  const photoContainer = similarCardTemplate.querySelector('.popup__photos');
+  photoContainer.innerHTML = '';
 };
-createPhotos();
+clearPhotosContainer();
 
 similarNotices.forEach((notice) => {
   const noticeElement = similarCardTemplate.cloneNode(true);
@@ -25,14 +18,18 @@ similarNotices.forEach((notice) => {
   noticeElement.querySelector('.popup__type').textContent = notice.offer.type;
   noticeElement.querySelector('.popup__text--capacity').textContent = `${notice.offer.rooms} комнаты для ${notice.offer.guests} гостей`;
   noticeElement.querySelector('.popup__text--time').textContent = `Заезд после ${notice.offer.checkin}, выезд до ${notice.offer.checkout}`;
-  noticeElement.querySelector('.popup__features').textContent = notice.offer.features; // ! нужно будет найти src картинок фич и отобразить их
+  noticeElement.querySelector('.popup__features').textContent = notice.offer.features;
   noticeElement.querySelector('.popup__description').textContent = notice.offer.description;
-  if (notice.offer.photos.length > 0) {
-    //const photosContainer = noticeElement.querySelector('.popup__photos');
-    //TODO Не ясен вывод
-    //const photos = createPhotos(notice);
-    //console.log(photos);
-    //photosContainer.appendChild(photos);
+  const photosContainer = noticeElement.querySelector('.popup__photos');
+  const sources = notice.offer.photos;
+  for (let i = 0; i < notice.offer.photos.length; i ++) {
+    const image = document.createElement('img');
+    image.src = sources[i];
+    image.classList.add('popup__photo');
+    image.alt = 'Фото жилья';
+    image.width = 45;
+    image.height = 40;
+    photosContainer.appendChild(image);
   }
   similarListFragment.appendChild(noticeElement);
 });
